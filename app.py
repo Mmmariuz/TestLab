a1027708bd631ed00e4e4cf64bc7df8b098b2830
Mmmariuz <mariushauge.93@gmail.com>
2025-08-02 14:36:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/fact", methods=["GET"])
+def give_fact():
+    return jsonify({"fact": "Flamingoer kan bare spise med hodet opp-ned!"})
+
+if __name__ == "__main__":
+    app.run()
 
