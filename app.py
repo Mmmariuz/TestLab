75a1b5e295c54c1d85e03e26621bef3d31e80358
Mmmariuz <mariushauge.93@gmail.com>
2025-08-22 19:48:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -7,11 +8,5 @@ def give_fact():
     return jsonify({"fact": "Flamingoer kan bare spise med hodet opp-ned!"})
 
 if __name__ == "__main__":
-    app.run()
-    
-import os
-
-port = int(os.environ.get("PORT", 5000))
-
-app.run(host="0.0.0.0", port=port)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
